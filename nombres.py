a4fd986e131f8675912508bc59867451fe223ad5
Jose Albornoz <jo.albornozs@duocuc.cl>
2024-06-13 19:27:39 -0400

    miprimercommit

---
+++ nombres.py
@@ -0,0 +1,21 @@
+edad=0
+nombre= input("ingrese su nombre y apellido: ")
+edad=int(input ("ingrese su edad: "))
+print("su nombre es :",nombre, "y tiene",edad,"años")
+
+frase=input("ingrese numeros enteros, separados por coma: ")
+
+print(frase.split(","))
+
+
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+
+x="python"
+y="is"
+z="awesome"
+usuario=input("complete la frase")
